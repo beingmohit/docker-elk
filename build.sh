641219f62b95e210c3adcf79e0beff9a6434a1d3
Mohit Agrawal <beingmohit09@gmail.com>
2018-11-13 13:26:34 +0530

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./clean.sh
 sudo sysctl -w vm.max_map_count=262144
-export KIBANA_CONNECT_RETRY=120
-export ES_CONNECT_RETRY=120
+export KIBANA_CONNECT_RETRY=12000
+export ES_CONNECT_RETRY=12000
 docker-compose up --build -d
 docker-compose logs -f -t
